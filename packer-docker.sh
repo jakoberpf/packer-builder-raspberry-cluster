d224ebe94d12dcdf92aace3dabcd2ae29c135f25
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2023-03-02 23:59:16 +0100

    rename wcluster script to packer-docker

--- packer-docker.sh
+++ packer-docker.sh
@@ -3,8 +3,8 @@
 set -e
 
 docker run -it --rm --privileged --cap-add=ALL -v /dev:/dev \
---mount type=bind,source="$(pwd)"/output,target=/app/output \
---mount type=bind,source="$(pwd)"/packer_cache,target=/app/packer_cache \
+--mount type=bind,source="$(pwd)"/.output,target=/app/output \
+--mount type=bind,source="$(pwd)"/.cache,target=/app/packer_cache \
 --mount type=bind,source="$(pwd)"/.ssh,target=/app/.ssh,readonly \
 --mount type=bind,source="$(pwd)"/.kube,target=/app/.kube,readonly \
 raspberry-cluster-tool " $@ "
