549c094e58ef3bc595223e85ba21e5ef6ba4a488
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2023-03-07 20:28:01 +0100

    remove packer docker script

---
+++ bin/packer-docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker run -it --rm --privileged --cap-add=ALL -v /dev:/dev \
+--mount type=bind,source="$(pwd)"/.output,target=/app/output \
+--mount type=bind,source="$(pwd)"/.cache,target=/app/packer_cache \
+--mount type=bind,source="$(pwd)"/.ssh,target=/app/.ssh,readonly \
+--mount type=bind,source="$(pwd)"/.kube,target=/app/.kube,readonly \
+raspberry-cluster-tool " $@ "
